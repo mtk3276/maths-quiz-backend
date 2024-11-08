78c809b5e387e732a1247d205cc64eb67a45a132
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-08 23:16:29 +0000

    feat: add seed file for populating questions in the database

---
+++ seed.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Question = require("./src/models/Question");
+
+mongoose.connect("mongodb://localhost:27017/maths-quiz");
+
+const seedQuestions = async () => {
+    const questions = [
+        { questionText: "15 + 31", correctAnswer: "46" },
+        { questionText: "22 - 14", correctAnswer: "8" },
+        { questionText: "92 - 6", correctAnswer: "86" },
+        { questionText: "24 + 27", correctAnswer: "51" },
+        { questionText: "17 - 5", correctAnswer: "12" },
+    ];
+
+    try {
+        await Question.deleteMany();
+        await Question.insertMany(questions);
+        console.log(`Database seeded with ${questions.length} questions.`);
+    } catch (error) {
+        console.error("Error seeding database:", error);
+    } finally {
+        mongoose.connection.close();
+    }
+};
+
+seedQuestions();
