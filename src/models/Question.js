7e11dc0a25cce1ad9bfd60991c41bb4fc6479fea
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-08 20:11:50 +0000

    feat: create Question model with schema for quiz questions

---
+++ src/models/Question.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+    questionText: {type: String, required: true},
+    correctAnswer: {type: Number, required: true},
+})
+
+module.exports = mongoose.model('Question', questionSchema);
