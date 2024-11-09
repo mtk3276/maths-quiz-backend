9ca579dbd6156bcd11aa970a968bbb4149c5a18b
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-09 01:54:53 +0000

    feat: update scoreController to prevent persisting multiple scores for the same username

--- src/controllers/scoreController.js
+++ src/controllers/scoreController.js
@@ -1,14 +1,20 @@
 const Score = require("../models/Score");
 
 exports.postScore = async (req, res) => {
-    let score = new Score({
-        username: req.body.username,
-        score: req.body.score,
-    });
+    const { username, score } = req.body;
 
     try {
-        await score.save();
-        res.status(201).send({ message: "Score added successfully" });
+        let existingScore = await Score.findOne({ username });
+
+        if (existingScore) {
+            existingScore.score = Math.max(existingScore.score, score);
+            await existingScore.save();
+            res.status(200).send({ message: "Score updated successfully" });
+        } else { 
+            const newScore = new Score ({ username, score });
+            await newScore.save();
+            res.status(201).send({ message: "Score added successfully" });
+        }
     } catch (error) {
         res.status(500).send({ message: error.message });
     }
